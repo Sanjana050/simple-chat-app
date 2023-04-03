0081bd22ccac5a3a2e7c5f0f33c26f562e440dd0
Sanjana050 <127759681+Sanjana050@users.noreply.github.com>
2023-04-04 00:14:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,84 @@
 const express=require('express');
 const app=express();
+const fs=require('fs')
+const path=require('path');
+
 const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/login',(req,res,next)=>{
+
+app.use('/login', (req, res, next) => {
+    res.sendFile('C:/Users/sanja/OneDrive/Desktop/tasks/views/login.html')
+  });
+
+
+
+
+  app.post('/',(req,res,next)=>{
     
-    res.send('<form action="/" method="POST"><input type="text" name="user" id="user"><button type="submit">LOGIN</button></form>')
-})
 
 
+    fs.writeFile('f1.txt',`${req.body.username}:${req.body.message}\n`,{flag:'a'},(err)=>{
+      if(err)
+      {
+        console.log('error')
+      }
+    })
 
-app.post("/",(req,res,next)=>{
+    res.redirect('/message')
+  })
+
+  
+
+
     
-    console.log(req.body);
-    res.redirect('/message');
+    
+    
 
-})
 
-app.use('/message',(req,res,next)=>{
-    if(req.body.message!="")
-    console.log(req.body);
-    res.send('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>')
+
+ app.use("/message",(req,res,next)=>{
+fs.readFile('C:/Users/sanja/OneDrive/Desktop/tasks/views/message.html','utf-8',(err,message)=>{
+if(err)
+{
+  console.log('errot')
+}
+else{
+  fs.readFile('f1.txt','utf-8',(err,data)=>{
+    if(err)
+    {
+      console.log('error')
+    }
+
+    else{
+      const com=message+'\n'+data;
+      res.send(com);
+    }
+  })
+}
 })
+  })
+
+
+
+    
+
+
+
+
+
+
 
 app.listen(80);
+
+
+
+
+
+
+
+       
+        
+
+    
+
